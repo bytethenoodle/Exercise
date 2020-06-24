11bc7b23c65a6ada2a54566bb47f28b15a782f71
Elbert John A. Orozco <elbertorozco@Elberts-MacBook-Pro.local>
2020-06-25 00:51:12 +0800

    Implement initial commit based on project generated by Xcode

---
+++ Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Exercise
+//
+//  Created by Elbert John A. Orozco on 6/25/20.
+//  Copyright © 2020 Elbert John A. Orozco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
