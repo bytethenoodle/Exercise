f421d17f88fa226d55ec139adaf1dfbad4b4034a
Elbert John A. Orozco <elbertorozco@Elberts-MacBook-Pro.local>
2020-06-25 13:57:00 +0800

    Implement alert cleanup

---
+++ Exercise/Core/General/Extensions/UIViewController.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewController.swift
+//  Exercise
+//
+//  Created by Elbert John A. Orozco on 6/25/20.
+//  Copyright © 2020 Elbert John A. Orozco. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func showAlertWithField(title: String, message: String, buttonActionTitle: String, action: @escaping (_ string: String?) -> Void) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        alert.addTextField(configurationHandler: nil)
+        alert.addAction(UIAlertAction(title: buttonActionTitle, style: .default, handler: { al in
+            let fieldString = alert.textFields?.first?.text
+            action(fieldString)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+}
--- Exercise/Scene/ToDoListViewController/ToDoListViewController.swift
+++ Exercise/Scene/ToDoListViewController/ToDoListViewController.swift
@@ -10,90 +10,85 @@ import UIKit
 
 class ToDoListViewController: BaseViewController<ToDoListViewModel, ToDoListCoordinator> {
 
+    // MARK: - Outlets
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var navigationButtonToDoItemAdd: UIBarButtonItem?
     
+    // MARK: - Controller Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
-        
         self.setupViewController()
-        
-        
-        
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-    }
-    
-    // MARK: - perform a binding from observableTodo from ViewModel to todoListTableView
+    // MARK: - Setups
     
     private func setupViewController() {
         DispatchQueue.main.async {
-            self.populateTodoListTableView()
-            self.setupTodoListTableViewCellWhenTapped()
-            self.setupTodoListTableViewCellWhenDeleted()
-            self.setupActionWhenButtonAddTodoTapped()
+            self.setupCellWhenSwipeTapped()
+            self.setupForAddAction()
+            self.populate()
         }
     }
     
-    private func populateTodoListTableView() {
-        
-        let observableToDoItems = viewModel?.get().asObservable()
-        let subscription = observableToDoItems?.bind(to: tableView.rx.items(cellIdentifier: String(describing: ToDoListItemCell.self),
-                                                         cellType: ToDoListItemCell.self)) { (row, toDoItem, cell) in
-            cell.setup(toDoItem: toDoItem)
-        }
-        subscription?.disposed(by: disposeBag)
-        
-        
+    private func setupCellWhenSwipeTapped() {
+        tableView.rx.setDelegate(self)
+                    .disposed(by: disposeBag)
     }
     
-    private func setupTodoListTableViewCellWhenTapped() {
-        tableView.rx.itemSelected
-            .subscribe(onNext: { indexPath in
-                self.tableView.deselectRow(at: indexPath, animated: false)
-                self.viewModel?.toggle(index: indexPath.row)
-            })
-            .disposed(by: disposeBag)
-    }
-    
-    private func setupTodoListTableViewCellWhenDeleted() {
-        tableView.rx.itemDeleted
-            .subscribe(onNext : { indexPath in
-                self.viewModel?.remove(index: indexPath.row)
-            })
-            .disposed(by: disposeBag)
-    }
-
-    // MARK: - event handling when add button tapped, and add todo to persistent storage via viewmodel
-    func setupActionWhenButtonAddTodoTapped() {
+    func setupForAddAction() {
         navigationButtonToDoItemAdd?.rx.tap
             .subscribe(
-                onNext: {
-                    let addTodoAlert = UIAlertController(title: "Add Todo", message: "Enter your string", preferredStyle: .alert)
-                    
-                    addTodoAlert.addTextField(configurationHandler: nil)
-                    addTodoAlert.addAction(UIAlertAction(title: "Add", style: .default, handler: { al in
-                        let todoString = addTodoAlert.textFields![0].text
-                        if !(todoString!.isEmpty) {
-                            self.viewModel?.add(title: todoString ?? String())
-                        }
-                    }))
-                    
-                    addTodoAlert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: nil))
-                    
-                    self.present(addTodoAlert, animated: true, completion: nil)
+                onNext: { [weak self] in
+                    self?.showAlertWithField(title: "Add Item",
+                                             message: "Adds a new item to the list. Setting the field empty invalidates the process and does nothing.",
+                                             buttonActionTitle: "Add",
+                                             action: { [weak self] (string) in
+                                                guard let fieldString = string else {return}
+                                                if !(fieldString.isEmpty) {
+                                                    self?.viewModel?.add(title: fieldString)
+                                                }
+                                             })
                 }
             )
             .disposed(by: disposeBag)
     }
-
-
+    
+    // MARK: - Table Refresh
+    
+    private func populate() {
+        let observableToDoItems = viewModel?.get().asObservable()
+        let subscription = observableToDoItems?
+            .bind(to: tableView.rx.items(cellIdentifier: String(describing: ToDoListItemCell.self),
+            cellType: ToDoListItemCell.self)) { (row, toDoItem, cell) in
+            cell.setup(toDoItem: toDoItem)
+        }
+        subscription?.disposed(by: disposeBag)
+    }
 }
 
+extension ToDoListViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.tableView.deselectRow(at: indexPath, animated: false)
+        self.viewModel?.toggle(index: indexPath.row)
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteButton = UITableViewRowAction(style: .destructive, title: "Delete") { [weak self] (action, indexPath) in
+                self?.viewModel?.remove(index: indexPath.row)
+                return
+            }
+
+        let editButton = UITableViewRowAction(style: .normal, title: "Edit") { (action, indexPath) in
+                
+                
+                
+                return
+            }
+            return [deleteButton, editButton]
+        }
+}
