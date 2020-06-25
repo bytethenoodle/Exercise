1fdd4a5bcc83e07af1b97da9577511820eaa3f84
Elbert John A. Orozco <elbertorozco@Elberts-MacBook-Pro.local>
2020-06-25 11:16:51 +0800

    Implement view model

--- Exercise/Scene/ToDoListTableViewController/ToDoListViewModel.swift
+++ Exercise/Scene/ToDoListTableViewController/ToDoListViewModel.swift
@@ -7,7 +7,48 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 class ToDoListViewModel: BaseViewModel {
     
+    // MARK: - Properties
+    
+    private var toDoItems = BehaviorRelay<[ToDoItem]>(value: [])
+    private var toDoItemAccessProvider = ToDoItemAccessProvider()
+    private var disposeBag = DisposeBag()
+    
+    // MARK: - Initializations
+    
+    required init() {
+        super.init()
+        self.fetchToDoItemsAndUpdateObservableToDoItems()
+    }
+    
+    // MARK: - Operations
+    
+    public func get() -> BehaviorRelay<[ToDoItem]> {
+        return toDoItems
+    }
+    
+    private func fetchToDoItemsAndUpdateObservableToDoItems() {
+        toDoItemAccessProvider.fetchObservable()
+            .map({ $0 })
+            .subscribe(onNext : { [weak self] (toDoItems) in
+                self?.toDoItems.accept(toDoItems)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    public func add(title: String) {
+        toDoItemAccessProvider.add(title: title)
+    }
+    
+    public func toggle(index: Int) {
+        toDoItemAccessProvider.toggle(index: index)
+    }
+    
+    public func remove(index: Int) {
+        toDoItemAccessProvider.remove(index: index)
+    }
 }
